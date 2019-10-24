35206484cc5082a6ceeaf850b9d75c2fb51cf1a6
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2019-10-24 17:01:50 -0400

    Added main() function and did minor cleanups.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from secant import secantMethod
 from newton import newtonsMethod
 from tableprinter.tableprinter import prettyTable
 from scipy.special import jv
+import sys
 
 #CONSTANTS
 ## Our good friend delta:
@@ -38,17 +39,26 @@ def getNewtonTable(guesses, epsilons):
 def generateNewtonTable(guesses, epsilons):
     return prettyTable(getNewtonTable(guesses, epsilons), epsilons, "Guess", "eps.")
 
-print("Table of relative errors for the truncated Bessel function:")
-print(prettyErrorTable(xVals, nVals, truncatedBessel))
-print("\n")
-print("Table of relative errors for the recursive Bessel function:")
-print(prettyErrorTable(xVals, nVals, dynamicBessel))
-print("\n"*3)
-print("Bisection method results for J_0:")
-print(generatePrettyTable(bisectionMethod, intervals, epsilons))
-print("\n")
-print("Secant method results for J_0:")
-print(generatePrettyTable(secantMethod, intervals, epsilons))
-print("\n")
-print("Newton's method results for J_0:")
-print(generateNewtonTable(newtonGuesses, epsilons))
+def returnTables():
+    print("Table of relative errors for the truncated Bessel function:")
+    print(prettyErrorTable(xVals, nVals, truncatedBessel))
+    print("\n")
+    print("Table of relative errors for the recursive Bessel function:")
+    print(prettyErrorTable(xVals, nVals, dynamicBessel))
+    print("\n"*3)
+    print("Bisection method results for J_0:")
+    print(generatePrettyTable(bisectionMethod, intervals, epsilons))
+    print("\n")
+    print("Secant method results for J_0:")
+    print(generatePrettyTable(secantMethod, intervals, epsilons))
+    print("\n")
+    print("Newton's method results for J_0:")
+    print(generateNewtonTable(newtonGuesses, epsilons))
+
+def main():
+    """ I'm having this just print the tables for now, but I'm leaving myself the option to
+        add functionality in the future. """
+    returnTables()
+
+if __name__ == '__main__':
+    main()
